d0ec9eb885b411ecb81b932e11926519d83ce9ef
Tomasz Mrowicki <tomrowicki@gmail.com>
2013-07-10 17:28:05 +0000

    new variable

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var fs = require ('fs');
 var message = fs.readFileSync(index.html);
+var buffer = new Buffer(message); 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(message);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
