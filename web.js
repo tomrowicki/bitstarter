9fe07adb14c50fa9299bae822fbfdc6d78fc21a4
Tomasz Mrowicki <tomrowicki@gmail.com>
2013-07-23 20:52:51 +0000

    Updated var app

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
   
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
