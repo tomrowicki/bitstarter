29dc4abdf7da3ed2b3da5d0cc041d3dff68afc58
Tomasz Mrowicki <tomrowicki@gmail.com>
2013-08-07 17:31:59 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var fs = require ('fs');
-var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
-  
 var express = require('express');
+var fs = require ('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+ var html = fs.readFileSync(htmlfile).toString();
+ response.send(html);
 });
 
 var port = process.env.PORT || 8080;
